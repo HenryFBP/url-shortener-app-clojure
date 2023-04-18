d8e498b6dd03ec4f7b58c570b05953d60db382ce
Henry Post <henryfbp@gmail.com>
2023-04-18 14:57:49 -0500

    finish db layer

--- src/shorturl/db.clj
+++ src/shorturl/db.clj
@@ -8,16 +8,16 @@
 (defn read-config [path]
   (edn/read-string (slurp path)))
 
-(read-config "deps.edn")
+;; (read-config "deps.edn")
 
-(read-config "db-creds.dev.edn")
+;; (read-config "db-creds.dev.edn")
 
 (def mysql-db (read-config "db-creds.dev.edn"))
 
-(defn query [q] 
+(defn query [q]
   (j/query mysql-db q))
 
-(defn insert! [q] 
+(defn insert! [q]
   (j/db-do-prepared mysql-db q))
 
 (defn get-redirects []
@@ -26,7 +26,30 @@
        (from :redirects)
        (sql/format))))
 
-(get-redirects)
+(defn get-url [slug]
+  (-> (query (->
+              (select :*)
+              (from :redirects)
+              (where [:= :slug slug])
+              (sql/format)))
+      first
+      :url))
 
+(comment
+  (->
+   [{:url "asdf.com"} {:a "a"} {:b "b"}]
+   first
+   :url))
+
+(defn insert-redirect! [slug url]
+  (insert! (-> (insert-into :redirects)
+               (columns :slug :url)
+               (values [[slug url]])
+               (sql/format))))
+
+(comment
+  (insert-redirect! "xyz" "https://www.youtube.com/watch?v=0mrguRPgCzI")
+  (get-redirects)
+  (get-url "xyz"))
 
 
